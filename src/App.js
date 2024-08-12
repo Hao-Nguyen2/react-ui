261a0082281e15d8ade5b8e700b66bd5bdeb2773
Hao-Nguyen2 <truongpro65k@gmial.com>
2024-08-12 20:46:22 +0700

    i sass

--- src/App.js
+++ src/App.js
@@ -1,40 +1,30 @@
 import { useState } from 'react';
 
 
-import './App.css';
-
 function App() {
+    const [job, setJob] = useState('');
+    const [jobs, setJobs] = useState([]);
 
-  const [job, setJob] = useState('');
-  const [jobs, setJobs] = useState([]);
+    const handleSubmit = () => {
+        // setJobs(prev =>{
+        //   return newJobs;
+        // } );
+        // setJob('');
+    };
 
+    return (
+        <div className="App">
+            <input value={job} onChange={(e) => setJob(e.target.value)} />
 
-  const handleSubmit = () => {
-    // setJobs(prev =>{
+            <button onClick={handleSubmit}>Add</button>
 
-
-
-    //   return newJobs;
-    // } );
-    // setJob('');
-  }
-
-  return (
-    <div className="App">
-      <input
-        value={job}
-        onChange={e => setJob(e.target.value)}
-      />
-
-      <button onClick={handleSubmit}>Add</button>
-
-      <ul>
-        {jobs.map((job, index) => (
-          <li key={index}>{job}</li>
-        ))}
-      </ul>
-    </div>
-  );
+            <ul>
+                {jobs.map((job, index) => (
+                    <li key={index}>{job}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
